141459344b52114d4ce104bcb72d181462950715
asnicho5 <91578416+asnicho5@users.noreply.github.com>
2024-03-06 15:05:47 -0700

    beautiful soup won't work
    beautiful soup won't work because it can't access dynamic javascript content which is how flightaware updates its website. We have to use selenium.

--- PHX-LV/Machine_Learning/webscraper.py
+++ PHX-LV/Machine_Learning/webscraper.py
@@ -1,10 +1,41 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#note as of 3/6/2024 14:56
+#this entire thing is obselete now because beautifulsoup can't do dynamically loaded content
+#it seems like the flight data is loaded in javascript
+#anyway now we have to use selenium 
+"""
 import requests
 from bs4 import BeautifulSoup
 import csv
 
+counter = 0
+
 url = 'https://www.flightaware.com/live/findflight?origin=kphx&destination=klax'
 
-response = requests.get(url)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0'
+}
+
+response = requests.get(url, headers=headers)
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -14,53 +45,76 @@ if response.status_code == 200:
     table = div_inside_big_div.find('table', id='Results')
     table_body = table.find('tbody')
     
-    allLinks = []
+    allLinksAndTime = []
     
     #conditions: the plane must be a B38M AND it must have arrived at the gate 
     for row in table_body.find_all('tr'):
-        aircraft_td = row.find('td', class='ffinder-results-aircraft')
-        arrived_td = row.find('td', class='ffinder-results-status')
-        status = arrived_td.text
-        aircraft_type = aircraft_td.text
+        tds = row.find_all('td')
+        print(tds)
+        
+        aircraft_td = row2.find('td', class_=')
+        arrived_or_not_td = row2.find('td', class_='ffinder-results-status')
+        arrived_or_not_span = arrived_or_not_td.find('span')
+        time_departed = row2.find('td', class_='ffinder-results-departure')
+        status = arrived_or_not_span.get_text()
+        aircraft_type = aircraft_td.get_text()
+        print("status:")
+        print(status)
+        print("aircraft_type:")
+        print(aircraft_type)
         if aircraft_td.text == "B38M":
             if "Arrive" in status:
-                ident_td = row.find('td', class='ffinder-result-ident')
+                ident_td = row.find('td', class_='ffinder-result-ident')
                 ident_span = ident_td.find('span')
                 ahref = ident_span.find('a')
                 link = ahref['href']
-                allLinks.append(link)
+                allLinks.append([link, time_departed.get_text()])
+                print("link:")
                 print(link)
+        
     
-    dataLinks = []
+    dataLinksAndTime = []
     
-    if allLinks:
-        for link in allLinks:
-            url = 'https://www.flightaware.com' + link
+    if allLinksAndTime:
+        for link in allLinksAndTime:
+            url = 'https://www.flightaware.com' + link[0]
             response = requests.get(url)
-            if response.status_code = 200:
+            if response.status_code == 200:
                 soup = BeautifulSoup(response.content, 'html.parser')
                 
                 nextLink_A = soup.find('a', id='trackLogLink')
                 nextLink = nextLink_A['href']
-                dataLinks.append(nextLink)
-                print(nextLink)
-                
-        if dataLinks:
-            for link in dataLinks:
-                url = 'https://www.flightaware.com' + link
+                dataLinksAndTime.append([nextLink, link[1]])
+                print("next link, link[1]:")
+                print([nextLink, link[1]])
+        
+        if dataLinksAndTime:
+            for link in dataLinksAndTime:
+                url = 'https://www.flightaware.com' + link[0]
                 response = requests.get(url)
                 
-                if response.status_code = 200:
+                if response.status_code == 200:
                     soup = BeautifulSoup(response.content, 'html.parser')
                     data = []
                     
                     table = soup.find('table', id='tracklogTable')
                     table_body = table.find('tbody')
-                    rows = table_body.find_all('tr', class=['smallrow1', 'smallrow2']):
+                    rows = table_body.find_all('tr', class_='smallrow1') + table_body.find_all('tr', class_='smallrow2')
                     
                     for row in rows:
-                        cells = [cell.text.strip() for cell in row
-                
-                
+                        cells = [cell.text.strip() for cell in row.find_all(['td'])]
+                        
+                        if cells:
+                            data.append(cells)
+                            
+                    output_filename = 'PHX_LV_' + counter + link[1]
+                    counter+= 1
+                    with open (output_filename, 'w', newline='') as csvfile:
+                        writer = csv.writer(csvfile)
+                        write.writerows(data)
 
+else:
+    print("big fail")
+                
+"""
             
