b2edef44e54cd436d6771dcdc8eb732b8c34c06a
Keya Gholap <113224365+Keya2418@users.noreply.github.com>
2024-04-14 19:27:45 -0700

    Add files via upload

--- PHX-LV/FinalUI/app.py
+++ PHX-LV/FinalUI/app.py
@@ -1,1 +1,50 @@
+from flask import Flask, render_template
+import csv
+import json
+import pandas as pd
+import math
 
+app = Flask(__name__, static_url_path='/static')
+
+@app.route('/')
+
+def index():
+    #latLongData = []
+    #altitudeData = []
+
+    flight_data = []
+
+    try:
+        excel_reader = pd.read_excel('Simbrief_Comparisons/PHX_LAS_022724_1942.xlsx')
+
+        for index, row in excel_reader.iterrows():
+            #skip the first two rows because they have stuff in them (the title and the departure time)
+            if index < 2:
+                continue
+            
+            longitude = float(row['Longitude'])
+            latitude = float(row['Latitude'])
+            height = float(row['feet'])
+
+            #this if statement checks to see if we made it to the very last row or not (or if any of the datapoints are faulty in some way)
+            if math.isnan(longitude) or math.isnan(latitude) or math.isnan(height):
+                continue
+
+            flight_data.append({"longitude": longitude, "latitude": latitude, "height": height})
+
+            #print(f"Latitude: {latitude}, Longitude: {longitude}, Height: {height}")
+                
+    except Exception as e:
+        print(f"An error has occurred: {e}")
+
+    #print(f"Flight data: {flight_data}")
+
+    return render_template('phx-las.html', flight_data=json.dumps(flight_data))
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
